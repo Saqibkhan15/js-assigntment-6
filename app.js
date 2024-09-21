dcffbef65334653e0f6175717df0058e4832f45c
Saqibkhan15 <aqib12u@gmail.com>
2024-09-22 01:53:18 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,55 @@
+1
+// var emptyArray = [];
+2
+// var multiArray = [
+//   [0, 1, 2, 3,],
+//   [1, 0, 1, 2,],
+//   [2, 1, 0, 1,],
+// ];
+// document.write(multiArray[0] + "<br>");
+// document.write(multiArray[1] + "<br>");
+// document.write(multiArray[2] + "<br>");
+3
+// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (var i = 0; i < numbers.length; i++) {
+//   document.write(numbers[i] + "<br>");
+// }
+4
+// var tableNumber = +prompt("Enter table number:");
+// var tableLength = +prompt("Enter table length:");
+// document.write("<h1>Multiplication Table of " + tableNumber + "</h1>");
+// for (var i = 1; i <= tableLength; i++) {
+//   document.write(tableNumber + " x " + i + " = " + (tableNumber * i) + "<br>");
+// }
+
+
+
+5
+// var fruits = ["apple", "banana", "mango", "orange", "strawberry"];
+// for (var i = 0; i < fruits.length; i++) {
+//   document.write("Element at index " + i + ": " + fruits[i] + "<br>");
+// }
+
+//6
+// document.write("Counting Series: <br>");
+// for (var i = 1; i <= 15; i++) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>Reverse Counting Series: <br>");
+// for (var i = 10; i >= 1; i--) {
+//   document.write(i + ", ");
+// }
+// document.write("<br><br>Even Series: <br>");
+// for (var i = 0; i <= 20; i += 2) {
+//   document.write(i + ", ");
+// }
+
+// document.write("<br><br>Odd Series: <br>");
+// for (var i = 1; i <= 20; i += 2) {
+//   document.write(i + ", ");
+// }
+
+// document.write("<br><br>2k Series: <br>");
+// for (var i = 2; i <= 20; i += 2) {
+//   document.write(i + "k, ");
+// }
